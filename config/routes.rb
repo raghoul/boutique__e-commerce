d784333c5c6819d3ad985735a0a27943672c969c
Hugo Calmels <calmels.hugo@yahoo.fr>
2021-06-08 03:08:32 +0200

    1.3

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  resources :items
   root to: 'landing#index'
  
   resources :landing, only: [:index, :show]
   devise_for :users
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/migrate/20210607222213_create_items.rb
+++ db/migrate/20210607222213_create_items.rb
@@ -5,6 +5,7 @@ class CreateItems < ActiveRecord::Migration[5.2]
       t.text :description
       t.decimal :price
       t.string :image_url
+      t.references :user, foreign_key: true
 
       t.timestamps
     end
